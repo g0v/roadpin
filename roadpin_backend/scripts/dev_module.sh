56cab5d37baecb1a15384fab578ab2c9ae4bc2d0
chhsiao <hsiao.chuanheng@gmail.com>
2013-06-08 15:21:41 +0800

    add roadpin_backend

---
+++ roadpin_backend/scripts/dev_module.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ "${BASH_ARGC}" != "1" ]
+then
+  echo "usage: scripts/dev.sh ${BASH_ARGV[0]}"
+  exit 0
+fi
+
+module=${BASH_ARGV[0]}
+
+module_without_leading_app=`echo "${module}"|sed 's/^app\.//g'`
+module_with_slash=`echo "${module_without_leading_app}"|sed 's/\./\//g'`
+
+echo "[INFO] to create module: ${module_with_slash}"
+pcreate -s simple_module ${module_with_slash} -d .
+
+pre_pkg=""
+arr=$(echo ${module}|tr "." "\n")
+for each_pkg in ${arr[@]}
+do
+  echo "each_pkg: ${each_pkg}"
+  if [ "${pre_pkg}" != "" ]
+  then
+    echo "[INFO] to create pkg: ${pre_pkg}"
+    pcreate -s pkg ${pre_pkg} -d .
+    pre_pkg="${pre_pkg}/"
+  fi
+  pre_pkg="${pre_pkg}${each_pkg}"
+done
---
+++ roadpin_backend/scripts/templates/web/app/util.py_tmpl
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import random
+import math
+import base64
+import time
+import ujson as json
+import traceback
+
+from app import cfg
+
+def trace(params):
+    traceback.print_stack()
