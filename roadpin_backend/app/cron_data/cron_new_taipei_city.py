d1f6ba0b02f4f84074ecb4ea5c67b98a32c8cd54
chhsiao <hsiao.chuanheng@gmail.com>
2013-06-23 22:06:01 +0800

    1. data with the_category/the_idx/the_id. 2. init cron_new_taipei_city

--- roadpin_backend/app/cron_data/cron_new_taipei_city.py
+++ roadpin_backend/app/cron_data/cron_new_taipei_city.py
@@ -15,7 +15,30 @@ from app import cfg
 from app import util
 
 def cron_new_taipei_city():
+    while True:
+        error_code = _cron_new_taipei_city()
+        _sleep()
     pass
+
+
+def _cron_new_taipei_city():
+    params = _get_params()
+    (error_code, results) = _crawl_data(params)
+    return error_code
+
+
+def _get_params():
+    '''
+    1. lookup the latest data in mongo.
+    2. return the latest params of data.
+    '''
+    latest_dig = util.get_cache('cron_new_taipei_city_latest_dig')
+
+
+def _sleep():
+    time_sleep = util._int(cfg.config.get('time_sleep', 3600))
+    cfg.logger.debug('to sleep: time_sleep: %s', time_sleep)
+    time.sleep(time_sleep)
 
 
 def parse_args():
