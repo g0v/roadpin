fa20e36e1486ae4abd6f4951983a1d0ac4869f72
chhsiao <hsiao.chuanheng@gmail.com>
2013-07-26 23:52:27 +0800

    roadpin deploy

--- roadpin_backend/scripts/deploy_install.sh
+++ roadpin_backend/scripts/deploy_install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if [ "${BASH_ARGC}" != 3 ]
+if [ "${BASH_ARGC}" != "4" ]
 then
-  echo "usage: deploy_install.sh [branch] [n_proc] [host]"
+  echo "usage: deploy_install.sh [ssh_key] [branch] [n_proc] [host]"
   exit 0
 fi
 
+ssh_key=${BASH_ARGV[3]}
 branch=${BASH_ARGV[2]}
 n_proc=${BASH_ARGV[1]}
 host=${BASH_ARGV[0]}
@@ -13,6 +14,4 @@ host=${BASH_ARGV[0]}
 echo "deploy_install.sh branch: ${branch} n_proc: ${n_proc} host: ${host}"
 
 echo "to install_stage1"
-fab -H ${host} -f deploy install.install_stage1:branch=${branch},n_proc=${n_proc}
-echo "to install_postinstall"
-fab -H ${host} -f deploy install.install_postinstall:n_proc=${n_proc}
+fab -i ${ssh_key} -H ${host} -f deploy install.install_stage1:branch=${branch},n_proc=${n_proc}
