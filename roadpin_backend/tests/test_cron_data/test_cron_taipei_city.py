931bb1177d73faeeadc080d5c251141135022fc1
chhsiao <hsiao.chuanheng@gmail.com>
2013-07-28 21:04:56 +0800

    geo compiled as LineString. (more complicated in polygon). [lon, lat] (based on mongo doc)

--- roadpin_backend/tests/test_cron_data/test_cron_taipei_city.py
+++ roadpin_backend/tests/test_cron_data/test_cron_taipei_city.py
@@ -24,3 +24,36 @@ class TestCronTaipeiCity(unittest.TestCase):
         '''a == 1 should not assert.'''
         logging.info("test_bottle_tmp: test_bottle_tmp_true")
         assert self.____never_used_variable == 1
+
+    def test__parse_geo(self):
+        the_data = {
+            'dtResultpro': [
+                {'GEO_TYPE': None,
+                 'POINTS': [
+                     {'P1': 121.53275726071669,
+                      'P2': 25.101171798153032},
+                     {'P1': 121.5329732086012,
+                      'P2': 25.1011710222917}]},
+                {'GEO_TYPE': 'Polygon',
+                 'POINTS': [
+                     {'P1': 121.52826916840115,
+                      'P2': 25.018342943987733},
+                     {'P1': 121.52885968944659,
+                      'P2': 25.018868265169033}]}]}
+        
+        result = cron_taipei_city._parse_geo(the_data)
+        logging.debug('result: %s', result)
+        assert len(result) == 2
+        assert result[0]['type'] == 'LineString'
+        assert len(result[0]['coordinates']) == 2
+        assert result[0]['coordinates'][0][0] == 121.53275726071669
+        assert result[0]['coordinates'][0][1] == 25.101171798153032
+        assert result[0]['coordinates'][1][0] == 121.5329732086012
+        assert result[0]['coordinates'][1][1] == 25.1011710222917
+
+        assert result[1]['type'] == 'LineString'
+        assert len(result[1]['coordinates']) == 2
+        assert result[1]['coordinates'][0][0] == 121.52826916840115
+        assert result[1]['coordinates'][0][1] == 25.018342943987733
+        assert result[1]['coordinates'][1][0] == 121.52885968944659
+        assert result[1]['coordinates'][1][1] == 25.018868265169033
