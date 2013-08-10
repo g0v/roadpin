2cea4388f1137ccc7ecc11805e43b8c94df7c683
chhsiao <hsiao.chuanheng@gmail.com>
2013-08-10 15:35:32 +0800

    g_search_by_location

--- roadpin_backend/app/http_handlers/g_search_by_location_handler.py
+++ roadpin_backend/app/http_handlers/g_search_by_location_handler.py
@@ -7,12 +7,20 @@ import base64
 import time
 import ujson as json
 
+from app.constants import EARTH_EQUATOIAL_CIRCUMFERENCE, EARTH_MERIDIONAL_CIRCUMFERENCE
+
 from app import cfg
 from app import util
 
 def g_search_by_location_handler(params):
-    lat = params.get('lat')
-    lon = params.get('lng')
+    lat = params.get('lat', 25)
+    lon = params.get('lng', 121)
+
+    dist = params.get('distance', 10)
+    dist = max(dist, 10)
+
+    dist_x = 360 * dist / EARTH_EQUATOIAL_CIRCUMFERENCE
+    dist_y = 360 * dist / EARTH_MERIDIONAL_CIRCUMFERENCE
 
     min_x = lon - dist_x
     max_x = lon + dist_x
