c12b8c8a7dc71d92ffd8cdbfba5a11f574e23df8
chhsiao <hsiao.chuanheng@gmail.com>
2013-08-10 14:36:04 +0800

    p_geo_handler

--- roadpin_backend/app/http_handlers/p_geo_handler.py
+++ roadpin_backend/app/http_handlers/p_geo_handler.py
@@ -11,16 +11,25 @@ from app import cfg
 from app import util
 
 def p_geo_handler(params):
-    the_timestamp = util.get_timestamp()
+    the_timestamp = util.get_milli_timestamp()
     user_id = params.get('user_id', '')
-    lat = params.get('lat', 0)
-    lon = params.get('lon', 0)
-    yaw = params.get('yaw', 0)
-    pitch = params.get('pitch', 0)
-    roll = params.get('roll', 0)
-    x = params.get('x', 0)
-    y = params.get('y', 0)
-    z = params.get('z', 0)
+    data = params.get('data', [])
+    for each_data in data:
+        _process_each_data(user_id, each_data, the_timestamp)
+
+
+def _process_each_data(user_id, each_data, server_timestamp):
+    offset_timestamp = each_data.get('offset_timestamp', 0)
+    lat = each_data.get('lat', 0)
+    lon = each_data.get('lon', 0)
+    yaw = each_data.get('yaw', 0)
+    pitch = each_data.get('pitch', 0)
+    roll = each_data.get('roll', 0)
+    x = each_data.get('x', 0)
+    y = each_data.get('y', 0)
+    z = each_data.get('z', 0)
+
+    the_timestamp = server_timestamp + offset_timestamp
 
     key = {'user_id': user_id, 'the_timestamp': the_timestamp}
     val = {'lat': lat, 'lon': lon, 'yaw': yaw, 'pitch': pitch, 'roll': roll, 'x': x, 'y': y, 'z': z}
