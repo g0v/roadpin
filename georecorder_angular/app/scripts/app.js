5bf0ec4967f3efa856776802251118759626acfb
chhsiao <hsiao.chuanheng@gmail.com>
2013-09-02 01:21:39 +0800

    1. init yeoman/bower/angular. TODO: 1. add jade. 2. add livescript or learn coffeescript

---
+++ georecorder_angular/app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('georecorderAngularApp', ['ngRoute'])
+  .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
