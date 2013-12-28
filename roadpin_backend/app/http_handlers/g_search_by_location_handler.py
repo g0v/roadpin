84f44ef0aabf84e27763bf5e6463090cf5dbd64e
chhsiao <hsiao.chuanheng@gmail.com>
2013-12-28 23:45:20 +0800

    no need to del _id in db results

--- roadpin_backend/app/http_handlers/g_search_by_location_handler.py
+++ roadpin_backend/app/http_handlers/g_search_by_location_handler.py
@@ -32,7 +32,5 @@ def g_search_by_location_handler(params):
     cfg.logger.debug('dist: %s dist_x: %s dist_y: %s to db_find: query: %s', dist, dist_x, dist_y, query)
 
     results = util.db_find('roadDB', query)
-    for result in results:
-        del result['_id']
     cfg.logger.debug('results: % len: %s', results, len(results))
     return results
