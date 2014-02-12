bbe89f277ff4286f699b705259439d27ac71da55
chhsiao <hsiao.chuanheng@gmail.com>
2014-02-13 00:05:31 +0800

    scripts_op/backend_server.sh add port

--- scripts_op/backend_server.sh
+++ scripts_op/backend_server.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-if [ "${BASH_ARGC}" != "2" ]
+if [ "${BASH_ARGC}" != "3" ]
 then
-  echo "usage: backend_server.sh [ini_filename] [log_filename]"
+  echo "usage: backend_server.sh [ini_filename] [log_filename] [port]"
   exit 0
 fi
 
-ini_filename=${BASH_ARGV[1]}
-log_dir=${BASH_ARGV[0]}
+ini_filename=${BASH_ARGV[2]}
+log_dir=${BASH_ARGV[1]}
+port=${BASH_ARGV[0]}
 
 cd roadpin_backend
 . __/bin/activate
