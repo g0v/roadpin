d014548c260fd94e54d9cd31df39049aa94f0039
chhsiao <hsiao.chuanheng@gmail.com>
2014-02-17 00:16:55 +0800

    frontend_server add port

--- scripts_op/frontend_server.sh
+++ scripts_op/frontend_server.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+if [ "${BASH_ARGC}" != "1" ]
+then
+  echo "usage: frontend_server.sh [port]"
+  exit 0
+fi
+
+port=${BASH_ARGV[0]}
+
 cd roadpin-frontend
 
 while [ 1 -eq 1 ]
 do
-  node_modules/brunch/bin/brunch watch -P --server
+  node_modules/brunch/bin/brunch watch -P --server -p ${port}
   sleep 1
 done
 
